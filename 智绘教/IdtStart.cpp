4a88e73eb15246257cc6172ce91d7e546996fbaa
Alan-CRL <alan-crl@foxmail.com>
2024-10-02 21:37:32 +0800

    20241002 2137 update

--- 智绘教/IdtStart.cpp
+++ 智绘教/IdtStart.cpp
@@ -1,5 +1,32 @@
 #include "IdtStart.h"
 
+#include "IdtDisplayManagement.h"
+
+struct
+{
+	int screenWidth;
+	int screenHeight;
+	int screenPhyWidth;
+	int screenPhyHeight;
+
+	int ramSize;
+}hardwareInfo;
+
 void StartForInkeys()
 {
+	// 获取基础信息
+	{
+		// 主显示器大小
+		hardwareInfo.screenWidth = MainMonitor.MonitorWidth;
+		hardwareInfo.screenHeight = MainMonitor.MonitorHeight;
+
+		// 主显示器物理尺寸
+		hardwareInfo.screenPhyWidth = MainMonitor.MonitorPhyWidth;
+		hardwareInfo.screenPhyHeight = MainMonitor.MonitorPhyHeight;
+
+		// 内存大小
+		MEMORYSTATUSEX memoryStatus;
+		memoryStatus.dwLength = sizeof(memoryStatus);
+		GlobalMemoryStatusEx(&memoryStatus);
+	}
 }
