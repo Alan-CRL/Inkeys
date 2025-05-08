2fddd64de92c7dd936ca42e1f40d82bb8999cb15
Alan-CRL <alan-crl@foxmail.com>
2025-05-08 12:50:50 +0800

    [auto]20250508 1250 update

--- 智绘教/IdtSetting.h
+++ 智绘教/IdtSetting.h
@@ -2,7 +2,7 @@
 #include "IdtMain.h"
 
 #include <d3d9.h>
-#include <C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include\D3dx9tex.h>
+#include "Microsoft DirectX SDK (June 2010)/Include/D3dx9tex.h"
 #pragma comment(lib, "d3d9")
 
 extern WNDCLASSEXW ImGuiWc;
