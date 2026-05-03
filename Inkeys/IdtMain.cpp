78d6e66a2cba7383f19b02892cd6ed76f0310354
Alan-CRL <alan-crl@foxmail.com>
2026-05-03 12:48:40 +0800

    20260503 1248 update
    20260503a Release

--- Inkeys/IdtMain.cpp
+++ Inkeys/IdtMain.cpp
@@ -53,7 +53,7 @@ import Inkeys.Other.Config;
 #pragma comment(lib, "netapi32.lib")
 
 wstring buildTime = __DATE__ L" " __TIME__;		// 构建时间
-wstring editionVersion = L"3.0.0-dev.76";		// 程序发布版本
+wstring editionVersion = L"3.0.0-dev.79";		// 程序发布版本
 wstring editionDate = L"20260503a";				// 程序发布日期
 
 wstring userId;									// 用户GUID
